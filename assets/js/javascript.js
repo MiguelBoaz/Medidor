59794a3718d963cf1c65839c416fd46eb4e0ab30
Miguel Boaz <migboaz@gmail.com>
2024-08-03 18:17:46 -0300

    Alterações no position e no email
    centralizei o form tanto no pc quanto no celular e fiz mandar email

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -11,16 +11,40 @@ const closeIcon = document.querySelector(".close");
 function playBar() {
     let gaymometer = Math.random() * 10;
     let gayVar = new Gay(nome.value, emailVar.value);
-
-    console.log(gaymometer);
-    bar.forEach ((barPar) => {barPar.style.width = `${gaymometer*10}%`});
-
-    setTimeout (() => {
-        popupText.innerText = `${gayVar.name} é ${Math.floor(gaymometer * 10)}% gay.`;
-        popupCard.classList.add('show');
-        overlay.classList.add('show');
-    }, 2000);
+    if(gayVar.email == "" ){
+        alert("Por favor insira um email válido.")
+    } else {
+        console.log(gaymometer);
+        bar.forEach ((barPar) => {barPar.style.width = `${gaymometer*10}%`});
     
+        setTimeout (() => {
+            popupText.innerText = `${gayVar.name} é ${Math.floor(gaymometer * 10)}% gay.`;
+            popupCard.classList.add('show');
+            overlay.classList.add('show');
+        }, 2000);
+    
+        const data = {
+            name: nome,
+            email: gayVar.email,
+            message: `${gayVar.name} é ${Math.floor(gaymometer * 10)}% gay.`,
+            bar_width: `${gaymometer*10}%`
+        };
+    
+        fetch('http://localhost:3000/send-email', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Sucesso:', data);
+        })
+        .catch((error) => {
+            console.error('Erro:', error);
+        });
+    }
 }
 
 function enterKey(key) {
@@ -49,5 +73,7 @@ button.addEventListener('click', playBar);
 nome.addEventListener('keyup', enterKey);
 emailVar.addEventListener('keyup', enterKey);
 closeIcon.addEventListener('click', close);
+
+
 
 // console.log(bar);
